b8d6502c3810137e17ae346cbc1f5c6021945638
samgarg7009 <samikshagarg321@gmail.com>
2023-05-29 00:17:21 +0530

    props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-//import * as info from './App';
+//import App from "./App";
+import Card from './Cards';
+
+
 ReactDOM.render(
   <>
-    <App></App>
+    <Card
+      imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
+      title="A Netflix Orignal Series"
+      sname="Dark"
+      link="https://www.netflix.com/in/title/80990668?sourse=35"
+    />
+    <Card
+      imgsrc="https://wallpapercave.com/uwp/uwp3621169.png"
+      title="A Netflix Orignal Series"
+      sname="Stranger Things"
+      link="https://www.netflix.com/in/title/80990668?sourse=35"
+    />
+    <Card
+      imgsrc="https://wallpapercave.com/wp/wp7418473.jpg"
+      title="A Netflix Orignal Series"
+      sname="Extra Cirricular"
+      link="https://www.netflix.com/in/title/80990668?sourse=35"
+    />
   </>,
   document.getElementById("root")
 );
