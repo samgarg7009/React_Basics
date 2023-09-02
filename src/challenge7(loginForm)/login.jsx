333c49c1f7fb50c0edeffe8d4caaa5ce9cf6ceff
samgarg7009 <samikshagarg321@gmail.com>
2023-09-03 00:22:03 +0530

    updated login form

--- src/challenge7(loginForm)/login.jsx
+++ src/challenge7(loginForm)/login.jsx
@@ -26,61 +26,66 @@ const App = () => {
         const { value, name } = event.target;
 
         SetFullName((preValue) => {
-            if (name === 'fName') {
-                return {
-                    fname: value,
-                    lname: preValue.lname,
-                    email: preValue.email,
-                    phno: preValue.phno,
-                };
-            } else if (name === 'lName') {
-                return {
-                    fname: preValue.fname,
-                    lname: value,
-                    email: preValue.email,
-                    phno: preValue.phno,
-                };
-            } else if (name === 'email') {
-                return {
-                    fname: preValue.fname,
-                    lname: preValue.lname,
-                    email: value,
-                    phno: preValue.phno,
-                };
-            } else if (name === 'phno') {
-                return {
-                    fname: preValue.fname,
-                    lname: preValue.lname,
-                    email: preValue.email,
-                    phno: value,
-                };
-            }
-        })
+            return {
+                ...preValue,
+                [name]: value,
+            };
+            //if (name === 'fName') {
 
-    }
+            //     return {
+            //         fname: value,
+            //         lname: preValue.lname,
+            //         email: preValue.email,
+            //         phno: preValue.phno,
+            //     };
+            // } else if (name === 'lName') {
+            //     return {
+            //         fname: preValue.fname,
+            //         lname: value,
+            //         email: preValue.email,
+            //         phno: preValue.phno,
+            //     };
+            // } else if (name === 'email') {
+            //     return {
+            //         fname: preValue.fname,
+            //         lname: preValue.lname,
+            //         email: value,
+            //         phno: preValue.phno,
+            //     };
+            // } else if (name === 'phno') {
+            //     return {
+            //         fname: preValue.fname,
+            //         lname: preValue.lname,
+            //         email: preValue.email,
+            //         phno: value,
+            //     };
+        }
+        )
+        }
 
-    const OnSubmit = (e) => {
-        e.preventDefault();
-    }
 
-    return (
-        <>
-            <div>
-                <form onSubmit={OnSubmit}>
-                    <div>
-                        <h1> hello {fullName.fname} {fullName.lname}</h1>
-                        <p>{fullName.email}</p>
-                        <p>{fullName.phno}</p>
-                        <input type='text' placeholder='enter your first name' name='fName' onChange={InputClicked} value={fullName.fname} />
-                        <input type='text' placeholder='enter your last name' name='lName' onChange={InputClicked} value={fullName.lname} />
-                        <input type='email' placeholder='enter your email' name='email' onChange={InputClicked} value={fullName.email} />
-                        <input type='number' placeholder='enter your phone number' name='phno' onChange={InputClicked} value={fullName.phno} />
-                        <button type="submit" >  submit 👍 </button>
-                    </div>
-                </form>
-            </div>
-        </>
-    );
+const OnSubmit = (e) => {
+    e.preventDefault();
+}
+
+return (
+    <>
+        <div>
+            <form onSubmit={OnSubmit}>
+                <div>
+                    <h1> hello {fullName.fname} {fullName.lname}</h1>
+                    <p>{fullName.email}</p>
+                    <p>{fullName.phno}</p>
+                    <input type='text' placeholder='enter your first name' name='fName' onChange={InputClicked} value={fullName.fname} />
+                    <input type='text' placeholder='enter your last name' name='lName' onChange={InputClicked} value={fullName.lname} />
+                    <input type='email' placeholder='enter your email' name='email' onChange={InputClicked} value={fullName.email} />
+                    <input type='number' placeholder='enter your phone number' name='phno' onChange={InputClicked} value={fullName.phno} />
+                    <button type="submit" >  submit 👍 </button>
+                </div>
+            </form>
+        </div>
+    </>
+);
 }
 
 
